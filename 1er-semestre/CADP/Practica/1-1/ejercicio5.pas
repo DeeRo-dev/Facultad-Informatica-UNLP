dfb55fd9eefa13eaf0f2a368a9ce883e4dfac303
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2025-04-03 09:48:20 -0300

    Update ejercicio5.pas

--- 1er-semestre/CADP/Practica/1-1/ejercicio5.pas
+++ 1er-semestre/CADP/Practica/1-1/ejercicio5.pas
@@ -1,22 +1,29 @@
-{5. Modifique el ejercicio anterior para que, luego de leer el número X, se lean a lo sumo 10 números
-reales. La lectura deberá finalizar al ingresar un valor que sea el doble de X, o al leer el décimo
-número, en cuyo caso deberá informarse “No se ha ingresado el doble de X”.}
+{
+    5. Modifique el ejercicio anterior para que, luego de leer el número X, se lean a lo sumo 10 números
+    reales. La lectura deberá finalizar al ingresar un valor que sea el doble de X, o al leer el décimo
+    número, en cuyo caso deberá informarse “No se ha ingresado el doble de X”.
+}
 
 program ejercicio5;
-var
-    x, num, cant: real;
+    var 
+        num1, num2, num3: real;
+        aux: integer;
 begin
-    writeln('Ingrese un numero real');
-    readln(x);
-    writeln('Ingrese otro numero real');
-    readln(num);
-    cant:= 1;
-    while(cant < 10) and (num <> x*2) do
-        begin
-            writeln('Ingrese otro numero real');
-            readln(num);
-            cant:= cant+1;
-        end;
-    if(cant = 10) then
-        writeln('No se ha ingresado el doble de X');
+  num2:= 0;
+  writeln ('Ingrese en el programa numeros  hasta que se ingrese uno cuyo valor sea exactamente el doble deel primer número leído');
+  write('Ingrese el primer numero: ');
+  readln(num1);
+  num3 := num1 * 2;
+  aux := 1;
+  while(num2 <> num3) and (aux <> 10) do
+    begin
+        writeln(aux);
+        writeln('Ingrese valores hasta indicar el doble de ', num1:0:2, ' : ');
+        readln(num2);
+        aux := aux + 1;
+    end;
+  if(aux <> 10 ) then
+    writeln('Genial, ingresaste ', num2:0:2, ', que es el doble de: ', num1:0:2)
+  else 
+     writeln('Llego al limite de ingresos de numeros, y no se ha ingresado el doble de', num1:0:2)
 end.
