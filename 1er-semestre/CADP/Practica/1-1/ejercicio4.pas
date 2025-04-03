63e9c61bd88d96f7fca207fea72600f4812e3071
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2025-04-03 08:57:02 -0300

    Update ejercicio4.pas

--- 1er-semestre/CADP/Practica/1-1/ejercicio4.pas
+++ 1er-semestre/CADP/Practica/1-1/ejercicio4.pas
@@ -1,17 +1,21 @@
-{4. Realizar un programa que lea un número real X. Luego, deberá leer números reales hasta que se
-ingrese uno cuyo valor sea exactamente el doble de X (el primer número leído).}
+{
+    4. Realizar un programa que lea un número real X. Luego, deberá leer números reales hasta que se
+    ingrese uno cuyo valor sea exactamente el doble de X (el primer número leído).
+}
 
-program ejercicio4;
-var
-    x, num: real;
+program Hello;
+    var 
+        num1, num2, num3: real;
 begin
-    writeln('Ingrese un numero real');
-    readln(x);
-    writeln('Ingrese otro numero real');
-    readln(num);
-    while(num <> x*2) do
-        begin
-            writeln('Ingrese otro numero real');
-            readln(num);
-        end;
+  num2:= 0;
+  writeln ('Ingrese en el programa numeros  hasta que se ingrese uno cuyo valor sea exactamente el doble deel primer número leído');
+  write('Ingrese el primer numero: ');
+  readln(num1);
+  num3 := num1 * 2;
+  while(num2 <> num3) do
+    begin
+        writeln('Ingrese valores hasta indicar el doble de ', num1:0:2, ' : ');
+        readln(num2);
+    end;
+   writeln('Genial, ingresaste ', num2:0:2, ', que es el doble de: ', num1:0:2)
 end.
