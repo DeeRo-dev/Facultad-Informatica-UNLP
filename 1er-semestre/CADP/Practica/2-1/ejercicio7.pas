92b9ea9287e8d0516f22e24783ffdef2189b8722
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-04-13 20:44:38 -0300

    Practica 2-1 realizada

--- 1er-semestre/CADP/Practica/2-1/ejercicio7.pas
+++ 1er-semestre/CADP/Practica/2-1/ejercicio7.pas
@@ -1,4 +1,6 @@
-{7. Dado el siguiente programa:
+{
+    7. Dado el siguiente programa:
+
 program alcanceYFunciones;
 var
     suma, cant : integer;
@@ -9,56 +11,33 @@ begin
     if (cant = 0) then
         prom := -1
     else
-        prom := suma / cant;
+        prom := suma / cant; //8
+    writeln(‘El promedio es: ’ , prom) 
+    calcularPromedio := prom // faltaba darle valor a la funcion   
 end;
 begin  //programa principal 
-    readln(suma);
-    readln(cant);
+    readln(suma); //48
+    readln(cant); //6
     if (calcularPromedio <> -1) then begin
         cant := 0;
-        writeln(‘El promedio es: ’ , calcularPromedio)
     end;
     else
         writeln(‘Dividir por cero no parece ser una buena idea’);
 end.
-a) La función calcularPromedio calcula y retorna el promedio entre las variables globales suma y cant, pero
-parece incompleta. ¿qué debería agregarle para que funcione correctamente?
-b) En el programa principal, la función calcularPromedio es invocada dos veces, pero esto podría mejorarse.
-¿cómo debería modificarse el programa principal para invocar a dicha función una única vez?
-c) Si se leen por teclado los valores 48 (variable suma) y 6 (variable cant), ¿qué resultado imprime el
-programa? Considere las tres posibilidades:
-i) El programa original
-ii) El programa luego de realizar la modificación del inciso a)
-iii) El programa luego de realizar las modificaciones de los incisos a) y b)} 
 
-{Inciso c)
-i) Promedio 0.00
-ii) Promedio -1.00
-iii) Promedio 8.00}
+}
 
-program alcanceYFunciones;
-var
-    suma, cant : integer;
-function calcularPromedio : real;
-var
-    prom : real;
-begin
-    if (cant = 0) then
-        prom := -1
-    else
-        prom := suma / cant;
-    calcularPromedio:= prom;
-end;
-var
-    prom: real;
-begin  //programa principal 
-    readln(suma);
-    readln(cant);
-    prom:= calcularPromedio;
-    if (prom <> -1) then begin
-        cant := 0;
-        writeln('El promedio es: ' , prom:0:2);
-    end
-    else
-        writeln('Dividir por cero no parece ser una buena idea');
-end.
+{
+    a) La función calcularPromedio calcula y retorna el promedio entre las variables globales suma y cant, pero
+       parece incompleta. ¿qué debería agregarle para que funcione correctamente?-
+    b) En el programa principal, la función calcularPromedio es invocada dos veces, pero esto podría mejorarse.
+       ¿cómo debería modificarse el programa principal para invocar a dicha función una única vez?-
+    c) Si se leen por teclado los valores 48 (variable suma) y 6 (variable cant), ¿qué resultado imprime el
+       programa? Considere las tres posibilidades:
+    
+        i) El programa original //0
+        ii) El programa luego de realizar la modificación del inciso a) //-1
+        iii) El programa luego de realizar las modificaciones de los incisos a) y b) //8
+} 
+
+
--- 1er-semestre/CADP/Practica/2-1/ejercicio8.pas
+++ 1er-semestre/CADP/Practica/2-1/ejercicio8.pas
@@ -1,4 +1,6 @@
-{8. Dado el siguiente programa:
+{
+    8. Dado el siguiente programa:
+
 program anidamientos;
 procedure leer;
 var
@@ -28,19 +30,21 @@ begin // programa principal
         writeln(‘Gracias, vuelva prontosss’);
 end.
 
-a) La función analizarLetra fue declarada como un submódulo dentro del procedimiento leer. Pero esto
-puede traer problemas en el código del programa principal.
-i) ¿qué clase de problema encuentra?
-ii) ¿cómo se puede resolver el problema para que el programa compile y funcione correctamente?
-b) La función analizarLetra parece incompleta, ya que no cubre algunos valores posibles de la variable letra.
-i) ¿De qué valores se trata?
-ii) ¿Qué sucede en nuestro programa si se ingresa uno de estos valores?
-iii) ¿Cómo se puede resolver este problema?}
+        a. La función analizarLetra fue declarada como un submódulo dentro del procedimiento leer, pero esto
+        puede traer problemas en el código del programa principal.
 
-{Inciso a) 
-i) El problema principal es que el programa principal llama a la funcion analizarLetra y esta solo es conocida en el procedimiento leer, por lo tanto da error.
-ii) Estaria bueno que la funcion sea conocida por todo el programa y no sea un submodulo para solucionar el error.
-Inciso b)
-i) El problema esta en que si el usuario ingresa un caracter que no sea una letra, la funcion no evalua este caso. Siempre retorna false, y no es una letra.
-b) Si el usuario no ingresa una letra la funcion siempre retorna false
-c) Este problema se puede solucionar por partes: primero se debe evaluar la condicion de que no sea una letra: podria ser una funcion que devuelve un entero y dependiendo que numero devuelva imprimo algo.}
+        i) ¿Qué clase de problema encuentra?
+            //El problema es que esta fuera del contexto del programa principal
+        ii) ¿Cómo se puede resolver el problema para que el programa compile y funcione correctamente?
+            //Se podria declarar la funcion por dentro del programa princiapl
+        b. La función analizarLetra parece incompleta, ya que no cubre algunos valores posibles de la variable
+        letra.
+
+        i) ¿De qué valores se trata?
+            //Se trata de otros caracteres que no sean de tipo letra
+        ii) ¿Qué sucede en nuestro programa si se ingresa uno de estos valores?
+            //Daria error ya que al ingresar por ejemplo @, la funcion no retorna un  valor real. Podria devolver siempre false
+        iii) ¿Cómo se puede resolver este problema?
+              //El programa deberia poder evaluar el tipo de caracter posible, por ejemplo tener una condicion mas, que evalue si no esta dentro de 'a .. z' o 'A..Z' 
+              imprima que no esta en el abecedario. Habria que analizar si la funcion es correcta o se cambia a un proceso.             
+}
