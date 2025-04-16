57f5c10bf3cc81a40a7f2b7313b176da0cad6633
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-04-15 23:03:53 -0300

    Practica 2-2 ejer. 5

--- 1er-semestre/CADP/Practica/2-2/ejercicio5.pas
+++ 1er-semestre/CADP/Practica/2-2/ejercicio5.pas
@@ -1,34 +1,36 @@
-{5. a. Realizar un módulo que reciba un par de números (numA,numB) y retorne si numB es el doble de numA.
-b. Utilizando el módulo realizado en el inciso a., realizar un programa que lea secuencias de pares de
-números hasta encontrar el par (0,0), e informe la cantidad total de pares de números leídos y la cantidad de
-pares en las que numB es el doble de numA.
-Ejemplo: si se lee la siguiente secuencia: (1,2) (3,4) (9,3) (7,14) (0,0) el programa debe informar los valores
-4 (cantidad de pares leídos) y 2 (cantidad de pares en los que numB es el doble de numA).}
+{
+    5. a. Realizar un módulo que reciba un par de números (numA,numB) y retorne si numB es el doble de numA.
+       b. Utilizando el módulo realizado en el inciso a., realizar un programa que lea secuencias de pares de
+          números hasta encontrar el par (0,0), e informe la cantidad total de pares de números leídos y la cantidad de
+          pares en las que numB es el doble de numA.
+          Ejemplo: si se lee la siguiente secuencia: (1,2) (3,4) (9,3) (7,14) (0,0) el programa debe informar los valores
+          4 (cantidad de pares leídos) y 2 (cantidad de pares en los que numB es el doble de numA).}
 
 program ejercicio5;
-function dobleNumero(numA, numB: integer): boolean;
-begin
-    dobleNumero:= (numB = (numA * 2));
-end;
-var
-    numA, numB, paresLeidos, paresDobles: integer;
-begin
-    writeln('Ingrese el numA');
-    readln(numA);
-    writeln('Ingrese el numB');
-    readln(numB);
-    paresLeidos:= 0;
-    paresDobles:= 0;
-    while(numA <> 0) or (numB <> 0) do
+    function isDouble(numA : integer; numB: integer) : boolean;
         begin
-            if(dobleNumero(numA, numB)) then
-                paresDobles:= paresDobles + 1;
-            paresLeidos:= paresLeidos+1;
-            writeln('Ingrese el numA');
-            readln(numA);
-            writeln('Ingrese el numB');
-            readln(numB);
+            if((numB * 2) = numA) then
+                isDouble := true
+            else
+                isDouble := false;    
         end;
-    writeln('Cantidad de pares leidos: ', paresLeidos);
-    writeln('Cantidad de pares en los que numB es el doble de numA: ', paresDobles);
+    var
+        num1, num2, cantNums : integer;
+    begin    
+        writeln('Enter numbers');
+        readln(num1);
+        readln(num2);
+        cantNums := 1;
+        while((num1 <> 0) and (num2 <> 0)) do
+            begin
+                if(isDouble(num1, num2)) then
+                    writeln('Value ' , num1 , ' is double to ', num2)
+                else 
+                    writeln('Value ' , num1 , ' not is double to ', num2);
+                readln(num1);
+                readln(num2);
+                cantNums := cantNums + 1;
+            end;
+        write('number of numbers read: ', cantNums);
+    end.
 end.
