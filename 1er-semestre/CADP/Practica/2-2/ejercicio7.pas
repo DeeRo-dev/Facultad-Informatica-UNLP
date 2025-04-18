0774ff395c80c47dd9c20e2c1bd00f1f02a5c86c
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-04-18 16:51:54 -0300

    Practica 2-2 ejercicio 7

--- 1er-semestre/CADP/Practica/2-2/ejercicio7.pas
+++ 1er-semestre/CADP/Practica/2-2/ejercicio7.pas
@@ -1,31 +1,49 @@
-{7. a. Realizar un módulo que reciba como parámetro un número entero y retorne la cantidad de dígitos que
-posee y la suma de los mismos.
-b. Utilizando el módulo anterior, realizar un programa que lea una secuencia de números e imprima la
-cantidad total de dígitos leídos. La lectura finaliza al leer un número cuyos dígitos suman exactamente 10, el
-cual debe procesarse.}
+{
+    7. 
+        a. Realizar un módulo que reciba como parámetro un número entero y retorne la cantidad de dígitos que
+        posee y la suma de los mismos.
+        b. Utilizando el módulo anterior, realizar un programa que lea una secuencia de números e imprima la
+        cantidad total de dígitos leídos. La lectura finaliza al leer un número cuyos dígitos suman exactamente 10, el
+        cual debe procesarse.
+}
 
 program ejercicio7;
-procedure cantDigitosSuma(num: integer; var cant, suma: integer);
+
+procedure calculate(num: integer; var cantDig: integer; var sum: integer; var sumDig:integer);
 var
-    dig: integer;
+    digito: integer;
 begin
-    while(num > 0) do
-        begin
-            cant:= cant + 1;
-            dig:= num mod 10;
-            suma:= suma + dig;
-            num:= num div 10;
-        end;
+    cantDig := 0;
+    sum := 0;
+    while (num <> 0) do 
+    begin
+        digito := num mod 10;
+        cantDig := cantDig + 1;
+        sumDig := sumDig + digito;
+        sum := sum + digito;
+        num := num div 10;
+    end;
 end;
+
 var
-    num, cant, suma: integer;
+    num, cantDigNum, sumNum, totalDigitos, sumDig: integer;
+    fin: boolean;
 begin
-    repeat
-        writeln('Ingrese una secuencia de numeros');
+    fin := false;
+    totalDigitos := 0;
+    sumDig := 0;
+    while (not fin) do
+    begin
+        write('Ingrese un número entero: ');
         readln(num);
-        cant:= 0;
-        suma:= 0;
-        cantDigitosSuma(num, cant, suma);
-        writeln('La cantidad total de digitos leidos es: ', cant);
-    until suma = 10;
+
+        calculate(num, cantDigNum, sumNum, sumDig);
+
+        totalDigitos := totalDigitos + cantDigNum;
+
+        if (sumNum = 10) then
+            fin := true;
+    end;
+
+    writeln('La cantidad total de dígitos ingresados es: ', totalDigitos, 'la suma de todos los digitos es de ', sumDig);
 end.
