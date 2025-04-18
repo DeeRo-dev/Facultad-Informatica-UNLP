f84180acc45a99f9f5a40eb1da79e28eeb69ae36
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-04-18 14:24:38 -0300

    practica 2-2 ejer 6 listo

--- 1er-semestre/CADP/Practica/2-2/ejercicio6.pas
+++ 1er-semestre/CADP/Practica/2-2/ejercicio6.pas
@@ -22,11 +22,13 @@ end;
 
 // Variables globales para los dos precios más bajos y sus códigos
 var
-    priceLow1, priceLow2: real;
-    codePriceLow1, codePriceLow2: integer;
+    priceLow1, priceLow2, pricePants: real;
+    codePriceLow1, codePriceLow2, codePants: integer;
 
-procedure calculate(code: integer; price: real);
+procedure calculate(code: integer; price: real; typeProduct: string; var count: integer; var aux:real);
 begin
+    if((typeProduct = 'pantalon') and (price > pricePants)) then
+        codePants := code;
     if (price < priceLow1) then
     begin
         priceLow2 := priceLow1;
@@ -40,26 +42,36 @@ begin
         priceLow2 := price;
         codePriceLow2 := code;
     end;
+    count := count + 1;
+    aux := aux + price;
 end;
 
 var
-    code, i: integer;
+    code, i, count: integer;
     typeProduct: string;
-    price: real;
+    price, average, aux: real;
 
 begin
     priceLow1 := 999999;
     priceLow2 := 999999;
     codePriceLow1 := -1;
     codePriceLow2 := -1;
-
-    for i := 1 to 4 do
+    codePants :=-1;
+    pricePants := -1;
+    aux := 0;
+    count := 0;
+    for i := 1 to 100 do
     begin
         writeln('Product ', i, ':');
         readProduct(code, typeProduct, price);
-        calculate(code, price);
+        calculate(code, price, typeProduct, count, aux);
     end;
-
+    average := aux / count;
     writeln('Codigo del producto MAS BARATO: ', codePriceLow1);
     writeln('Codigo del SEGUNDO producto mas barato: ', codePriceLow2);
+    if codePants <> -1 then
+        writeln('Codigo del pantalon mas caro es: ', codePants)
+    else
+      writeln('No se ingresaron pantalones.');
+    writeln('El promedio de los precios ingresados son de: ', average:0:2);
 end.
