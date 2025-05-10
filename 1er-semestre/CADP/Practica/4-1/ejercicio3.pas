86172179b6f32dd1cb99e0eb62b7a8b42713e163
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-05-10 14:15:34 -0300

    Ejercicio 3 prac 4

--- 1er-semestre/CADP/Practica/4-1/ejercicio3.pas
+++ 1er-semestre/CADP/Practica/4-1/ejercicio3.pas
@@ -1,95 +1,104 @@
-{3. Se dispone de un vector con números enteros, de dimensión física dimF y dimensión lógica dimL.
-a) Realizar un módulo que imprima el vector desde la primera posición hasta la última.
-b) Realizar un módulo que imprima el vector desde la última posición hasta la primera.
-c) Realizar un módulo que imprima el vector desde la mitad (dimL DIV 2) hacia la primera posición, y
-desde la mitad más uno hacia la última posición.
-d) Realizar un módulo que reciba el vector, una posición X y otra posición Y, e imprima el vector desde la
-posición X hasta la Y. Asuma que tanto X como Y son menores o igual a la dimensión lógica. Y considere
-que, dependiendo de los valores de X e Y, podría ser necesario recorrer hacia adelante o hacia atrás.
-e) Utilizando el módulo implementado en el inciso anterior, vuelva a realizar los incisos a, b y c}
+{
 
-program ejercicio3;
+    3. Se dispone de un vector con números enteros, de dimensión física dimF y dimensión lógica dimL.
+        a. Realizar un módulo que imprima el vector desde la primera posición hasta la última.
+        b. Realizar un módulo que imprima el vector desde la última posición hasta la primera.
+        c. Realizar un módulo que imprima el vector desde la mitad (dimL DIV 2) hacia la primera posición, y
+           desde la mitad más uno hacia la última posición.
+        d. Realizar un módulo que reciba el vector, una posición X y otra posición Y, e imprima el vector desde
+            la posición X hasta la Y. Asuma que tanto X como Y son menores o iguales a la dimensión lógica. Y
+            considere que, dependiendo de los valores de X e Y, podría ser necesario recorrer hacia adelante o
+            hacia atrás.
+        e. Utilizando el módulo implementado en el inciso anterior, vuelva a realizar los incisos a, b y c.
+}
+
+
+program Vectores;
 const
-	dimF = 30;
+    cant_datos = 5;
 type
-	vector = array [1..dimF] of integer;
-procedure cargarVector(var v: vector; var dimL: integer); //Se dispone
+    vdatos = array[1..cant_datos] of real;
+    
+procedure cargarVector(var v:vdatos; var dimL : integer);
 var
-	num: integer;
+    num : real;
 begin
-	num:= Random(15);
-	while((dimL < dimF) and (num <> 0)) do
-		begin
-			dimL:= dimL + 1;
-			v[dimL]:= num;
-			num:= Random(15);
-		end;
+    dimL:=0;
+    readln(num);
+    while((dimL < cant_datos) and (num <> 0)) do
+        begin
+            dimL := dimL + 1;
+            v[dimL] := num;
+           if (dimL < cant_datos) then 
+                begin
+                    writeln('Ingrese otro valor de tipo real (0 para terminar):');
+                    readln(num);
+                end;
+        end;
 end;
-procedure imprimirVectorOrden(v: vector; dimL: integer);
-var
-	i: integer;
+procedure leerVector(v:vdatos; dimL:integer);
+var 
+    i: integer;
 begin
-	for i:= 1 to dimL do
-		write(v[i], ' | ');
+    writeln('-------- Lectura en asc --------');
+    for i := 1 to dimL do
+        begin
+            writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+        end;
+    writeln('------ Lectura en dsc -----------');    
+    for i := dimL downto 1 do
+        begin
+            writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+        end;
+    writeln('------ Lectura en mitad dsc -----------');    
+    for i := (dimL DIV 2) downto 1 do
+        begin
+            writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+        end;
 end;
-procedure imprimirVectorInvertido(v: vector; dimL: integer);
+procedure leerVectorXandY(v:vdatos; x:integer; y:integer);
 var
-	i: integer;
+    i:integer;
 begin
-	for i:= dimL downto 1 do
-		write(v[i], ' | ');
+    if(x < y) then
+        begin
+            for i := x to y do
+                begin
+                    writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+                end;
+              writeln('------ Lectura dimL DIV 2 asc -----------');        
+            for i := (dimL DIV 2) to y do
+        begin
+            writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+        end;
+        end
+    else if(x > y) then
+        begin
+            for i := x downto y do
+                begin
+                    writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+                end;
+            writeln('------ Lectura dimL DIV 2 dsc -----------');            
+            for i := (dimL DIV 2) downto y do
+        begin
+            writeln('El valor en la posición ', i, ' es de: ', v[i]:0:2);
+        end;    
+        end
+    else
+        writeln('Se eligio la misma posicion, el valor es: ', v[x]:0:2);
 end;
-procedure imprimirVectorMitad(v: vector; dimL: integer);
 var
-	i: integer;
-	mitad: integer;
+    datos : vdatos;
+    dim, posicionX, posicionY : integer;
+    
 begin
-	mitad:= dimL DIV 2;
-	writeln('Primera mitad:');
-	for i:= 1 to mitad do
-		write(v[i], ' | ');
-    writeln();
-	writeln('Segunda mitad: ');
-	for i:= mitad+1 to dimL do
-		write(v[i], ' | ');
-end;
-procedure imprimirEntreValores(v: vector; x, y: integer);
-var
-	i: integer;
-begin
-	if(x > y) then
-		for i:= x downto y do
-			write(v[i], ' | ')
-	else
-		for i:= x to y do
-			write(v[i], ' | ');
-end;
-var
-	v: vector;
-	dimL, x, y: integer;
-begin
-    Randomize;
-	dimL:= 0;
-	cargarVector(v, dimL);
-	writeln('DIML=', dimL);
-	imprimirVectorOrden(v, dimL);
-    writeln();
-	imprimirVectorInvertido(v, dimL);
-    writeln();
-	imprimirVectorMitad(v, dimL);
-    writeln();
-	writeln('Ingrese un valor x');
-	readln(x);
-	writeln('Ingrese un valor y');
-	readln(y);
-	imprimirEntreValores(v, x, y);
-    writeln();
-    writeln('Reutilizando el modulo entre valores');
-	imprimirEntreValores(v, 1, dimL);
-    writeln();
-	imprimirEntreValores(v, dimL, 1);
-    writeln();
-	imprimirEntreValores(v, 1, dimL div 2);
-    writeln();
-	imprimirEntreValores(v, dimL div 2 + 1, dimL);
+    dim := 0;
+    cargarVector(datos, dim);
+    leerVector(datos, dim);
+    writeln('Elija una posicion para leer el vector: ');
+    writeln('Posicion x:' );
+    readln(posicionX);
+    writeln('Posicion y:' );
+    readln(posicionY);
+    leerVectorXandY(datos, posicionX, posicionY);
 end.
