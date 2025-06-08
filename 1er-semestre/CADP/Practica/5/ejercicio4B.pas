7dbaecaad30ed4df1b3955bcc84db69f7168d411
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-06-08 09:39:54 -0300

    Pract 5 : Ejer 4B

--- 1er-semestre/CADP/Practica/5/ejercicio4B.pas
+++ 1er-semestre/CADP/Practica/5/ejercicio4B.pas
@@ -1,31 +1,19 @@
 {4) b) Indicar los valores que imprimen los siguientes programas en Pascal. Justificar mediante prueba de
 escritorio.
+
 program punteros;
 type
-    cadena = string[50];
-    puntero_cadena = ^cadena;
+    cadena = string[50]; // 50 + 1 = 51
+    puntero_cadena = ^cadena; 
 var
-    pc: puntero_cadena;
+    pc: puntero_cadena; //= 4 memoria estatica
 begin
     new(pc);
     pc^:= 'un nuevo nombre';
-    writeln(sizeof(pc^), ' bytes');
+    writeln(sizeof(pc^), ' bytes'); //51 en memoria dinamica
     writeln(pc^);
-    dispose(pc);
-    pc^:= 'otro nuevo nombre';
-end.}
-
-program ejercicio4B;
-type
-    cadena = string[50];
-    puntero_cadena = ^cadena;
-var
-    pc: puntero_cadena;
-begin
-    new(pc);
-    pc^:= 'un nuevo nombre';
-    writeln(sizeof(pc^), ' bytes'); //51 bytes
-    writeln(pc^); //un nuevo nombre
-    dispose(pc);
-    pc^:= 'otro nuevo nombre'; //Esta linea da error, ya que se libero la posicion de memoria apuntada por pc, al haber hecho en la linea anterior el dispose de este, no pudiendo acceder a esta direccion
 end.
+
+
+.}
+
