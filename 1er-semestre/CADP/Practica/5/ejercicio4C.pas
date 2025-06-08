06234cf84f983d19602ddb8536e6f9e6ac31520b
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-06-07 21:20:29 -0300

    Pract. 5: ejerc 4C

--- 1er-semestre/CADP/Practica/5/ejercicio4C.pas
+++ 1er-semestre/CADP/Practica/5/ejercicio4C.pas
@@ -4,24 +4,7 @@ program punteros;
 type
     cadena = string[50];
     puntero_cadena = ^cadena;
-procedure cambiarTexto(pun: puntero_cadena);
-begin
-    pun^:= 'Otro texto';
-end;
-var
-    pc: puntero_cadena;
-begin
-    new(pc);
-    pc^:= 'Un texto';
-    writeln(pc^);
-    cambiarTexto(pc);
-    writeln(pc^);
-end.}
 
-program ejercicio4C;
-type
-    cadena = string[50];
-    puntero_cadena = ^cadena;
 procedure cambiarTexto(pun: puntero_cadena);
 begin
     pun^:= 'Otro texto';
@@ -31,7 +14,9 @@ var
 begin
     new(pc);
     pc^:= 'Un texto';
-    writeln(pc^); //Un texto
+    writeln(pc^); //un texto
     cambiarTexto(pc);
-    writeln(pc^); //Otro texto. Al ser punteros, el contenido apuntado siempre se ve modificado a pesar de que el parametro haya sido pasado por valor. La copia apunta a la misma direccion de memoria
+    writeln(pc^); // Otro texto, dado que apunta a l a variable dinamica, se modifica el valor en el procso
 end.
+}
+
