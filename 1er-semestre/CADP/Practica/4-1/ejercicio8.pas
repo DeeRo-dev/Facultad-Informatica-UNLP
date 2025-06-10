0126f34e125a8ca3716c86a068f569f8e98e7940
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-06-09 22:32:04 -0300

    prac 4 : ejer8

--- 1er-semestre/CADP/Practica/4-1/ejercicio8.pas
+++ 1er-semestre/CADP/Practica/4-1/ejercicio8.pas
@@ -1,61 +1,55 @@
 {
-    8. Realizar un programa que lea y almacene la información de 400 alumnos ingresantes a la Facultad de
-        Informática de la UNLP en el año 2020. De cada alumno se lee: nro de inscripción, DNI, apellido, nombre y
-        año de nacimiento. Una vez leída y almacenada toda la información, calcular e informar:
-        
-        a) El porcentaje de alumnos con DNI compuesto sólo por dígitos pares.
-        b) Apellido y nombre de los dos alumnos de mayor edad.
+    Realizar un programa que lea y almacene la información de 400 alumnos ingresantes a la Facultad de
+    Informática de la UNLP en el año 2020. De cada alumno se lee: nro de inscripción, DNI, apellido,
+    nombre y año de nacimiento. Una vez leída y almacenada toda la información, calcular e informar:
+    
+    a. El porcentaje de alumnos con DNI compuesto sólo por dígitos pares.
+    
+    b. Apellido y nombre de los dos alumnos de mayor edad.
+
 }
 
+
 program ejercicio8;
-const 
-    dimF = 400;
+const DF = 400;
 type
-    registroAlum = record 
+    dataAlumno = record
         numInscripcion: integer;
-        dni:integer;
+        dni: integer;
         apellido: string;
         nombre: string;
-        anioNacimientto: integer;
+        nacimiento: integer;
     end;
-    registro = array [1..dimF] of registroAlum;
-procedure leerAlumn(var infoAlumn: registroAlum);
+    registros = array [1.. DF] of dataAlumno;
+procedure cargarAlumnos(var datos: registros);
+
+var
+    i: integer;
+    data: dataAlumno;
+begin
+    for i := 1 to DF do
     begin
-        write('Ingrese el numero de inscripción: ');
-        readln(infoAlumn.numInscripcion);
-        write('Ingrese el DNI: ');
-        readln(infoAlumn.dni);
-        write('Ingrese el apellido: ');
-        readln(infoAlumn.apellido);
-        write('Ingrese el nombre: ');
-        readln(infoAlumn.nombre);
-        write('Ingrese el anio de nacimiento: ');
-        readln(infoAlumn.anioNacimientto);
-        writeln('--- ingrese un nuevo alumno----');
-    end;
-procedure leerRegistro(var v:registro);
-var
-    i:integer;
-    dataAlumn : registroAlum;
-begin
-    for i := 1 to dimF do
-        begin
-            leerAlumn(dataAlumn); 
-            v[i]:= dataAlumn;
+        writeln('-----INGRESE LOS DATOS DEL ALUMNO--------');
+        write('Ingrese el numero de inscripción: ');   
+        readln(data.numInscripcion);
+        write('Ingrese el nombre: ');   
+        readln(data.nombre);
+        write('Ingrese el apellido: ');   
+        readln(data.apellido);
+        write('Ingrese el dni: ');   
+        readln(data.dni);
+        write('Ingrese el numero de nacimiento: ');   
+        readln(data.nacimiento);
+        datos[i]:= data;
         end;
 end;
-procedure procesarInfo(v:registro);
-var
-    i:integer;
+procedure porcentaje(data: registros);
+begin
     
-begin
-    for i := 1 to dimF do
-        begin
-            v[i]
-        end;
 end;
-var 
-    registroFacultad: registro;
+var
+    datos: registros;
 begin
-    leerRegistro(registroFacultad);
+     writeln('-----REGISTRO DE ALUMNOS DE LA FACULTAD--------');
+    cargarAlumnos(datos);
 end.
