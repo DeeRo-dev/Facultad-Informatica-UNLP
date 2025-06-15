787f1ccb63f2484c24ca3dfa4d57b185bf3946fa
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-06-15 11:38:09 -0300

    Practica 6, ejercicio 4

--- 1er-semestre/CADP/Practica/6/ejercicio4.pas
+++ 1er-semestre/CADP/Practica/6/ejercicio4.pas
@@ -1,8 +1,13 @@
-{4. Utilizando el programa del ejercicio 1, realizar los siguientes módulos:
-a. Máximo: recibe la lista como parámetro y retorna el elemento de valor máximo.
-b. Mínimo: recibe la lista como parámetro y retorna el elemento de valor mínimo.
-c. Múltiplos: recibe como parámetros la lista L y un valor entero A, y retorna la cantidad de
-elementos de la lista que son múltiplos de A.}
+{
+    4. Utilizando el programa del ejercicio 1, realizar los siguientes módulos:
+    
+    a. Máximo: recibe la lista como parámetro y retorna el elemento de valor máximo.
+    
+    b. Mínimo: recibe la lista como parámetro y retorna el elemento de valor mínimo.
+    
+    c. Múltiplos: recibe como parámetros la lista L y un valor entero A, y retorna la cantidad de
+    elementos de la lista que son múltiplos de A.
+}
 
 program ejercicio4;
 type
@@ -20,82 +25,79 @@ begin
     aux^.sig := L;
     L := aux;
 end;
-procedure imprimirLista(l: lista);
-begin
-    while(l<>nil) do 
-        begin
-            writeln(l^.num);
-            l:= l^.sig;
-        end;
-end;
-procedure incrementarLista(l: lista; num: integer);
-begin
-    while(l<>nil) do
-        begin
-            l^.num:= l^.num + num;
-            l:= l^.sig;
-        end;
-end;
-function maximoLista(l: lista): integer;
+procedure incrementar(var l: lista; num: integer);
 var
-	max: integer;
+    aux: lista;
 begin
-	max:= -1;
-	while(l <> nil) do
-		begin
-			if(l^.num > max) then	
-				max:= l^.num;
-			l:= l^.sig;
-		end;
-	maximoLista:= max;
+    aux := l;
+    while(aux <> nil) do
+    begin
+        aux^.num := aux^.num + num;
+        aux := aux^.sig;
+    end;
 end;
-function minimoLista(l: lista): integer;
-var
-	min: integer;
+procedure leerLista(l: lista);
 begin
-	min:= 9999;
-	while(l <> nil) do
-		begin
-			if(l^.num < min) then	
-				min:= l^.num;
-			l:= l^.sig;
-		end;
-	minimoLista:= min;
+    while(l <> nil) do
+    begin
+        writeln('El contenido de la lista es: ', l^.num);
+        l := l^.sig;
+    end;
 end;
-function multiplosLista(l: lista; a: integer): integer;
-var
-	cant: integer;
+procedure maxi(l:lista; var max: integer);
 begin
-	cant:= 0;
-	while(l <> nil) do
-		begin
-			if(l^.num mod a = 0) then
-				cant:= cant + 1;
-			l:= l^.sig;
-		end;
-	multiplosLista:= cant;
+    max:= -1; 
+    while(l <> nil) do 
+    begin
+        if(l^.num > max) then
+            max := l^.num;
+        l := l^.sig;
+    end;
+end;
+procedure mini(l:lista; var min: integer);
+begin
+    min:= 9999; 
+    while(l <> nil) do 
+    begin
+        if(l^.num < min) then
+            min := l^.num;
+        l := l^.sig;
+    end;
+end;
+procedure multiples(l:lista; multiplo: integer; var contador: integer);
+begin
+    while(l <> nil ) do
+    begin
+        if((l^.num MOD multiplo) = 0) then
+            contador := contador + 1;
+        l := l^.sig;
+    end;
 end;
 var
     pri : lista;
-    valor : integer;
+    valor, inc, max, min, multiplo, contador: integer;
 begin
     pri := nil;
+    contador := 0;
     writeln('Ingrese un numero');
     read(valor);
-    while (valor <> 0) do begin
+    while (valor <> 0) do
+    begin
         armarNodo(pri, valor);
-        writeln('Ingrese un numero');
-    read(valor);
+        read(valor);
+        writeln('‘Ingrese un numero’');
     end;
-    imprimirLista(pri);
-    {writeln('Ingrese un valor');
-    readln(valor);
-    writeln('Lista actualizada:');
-    incrementarLista(pri, valor);
-    imprimirLista(pri);}
-    writeln('El elemento de valor maximo en la lista es: ', maximoLista(pri));
-    writeln('El elemento de valor minimo en la lista es: ', minimoLista(pri));
-    writeln('Ingrese un valor entero A');
-    readln(valor);
-    writeln('La cantidad de elementos de la lista que son multiplos de A es: ', multiplosLista(pri, valor));
+    leerLista(pri);
+    writeln('Íngrese un valor para incrementar cada dato de la lista: ');
+    read(inc);
+    incrementar(pri, inc);
+    leerLista(pri);
+    maxi(pri, max);
+    writeln('El numer maximo leido es el : ', max);
+    mini(pri, min);
+    writeln('El numer minimo leido es el : ', min);
+    write('Ingrese un numero para saber cuantos en la lista son multiplos: ');
+    readln(multiplo);
+    multiples(pri, multiplo, contador);
+    writeln('La cantidad de multiplos es de : ', contador);
 end.
