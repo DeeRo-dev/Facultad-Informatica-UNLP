e49ac6270aff47fcaab3c7e3678a5ef25d4372d2
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-06-18 10:04:54 -0300

    Practica 4, ejerc 10

--- 1er-semestre/CADP/Practica/4-1/ejercicio10.pas
+++ 1er-semestre/CADP/Practica/4-1/ejercicio10.pas
@@ -1,58 +1,88 @@
-{10. Realizar un programa que lea y almacene el salario de los empleados de una empresa de turismo (a lo
-sumo 300 empleados). La carga finaliza cuando se lea el salario 0 (que no debe procesarse) o cuando se
-completa el vector. Una vez finalizada la carga de datos se pide:
-a) Realizar un módulo que incremente el salario de cada empleado en un 15%. Para ello, implementar un
-módulo que reciba como parámetro un valor real X, el vector de valores reales y su dimensión lógica y
-retorne el mismo vector en el cual cada elemento fue multiplicado por el valor X.
-b) Realizar un módulo que muestre en pantalla el sueldo promedio de los empleados de la empresa.}
+{
+	10. Realizar un programa que lea y almacene el salario de los empleados de una empresa de turismo (a lo
+	sumo 300 empleados). La carga finaliza cuando se lea el salario 0 (que no debe procesarse) o cuando se
+	completa el vector. Una vez finalizada la carga de datos se pide:
+
+	a) Realizar un módulo que incremente el salario de cada empleado en un 15%. Para ello, implementar un
+	módulo que reciba como parámetro un valor real X, el vector de valores reales y su dimensión lógica y
+	retorne el mismo vector en el cual cada elemento fue multiplicado por el valor X.
+
+	b) Realizar un módulo que muestre en pantalla el sueldo promedio de los empleados de la empresa.
+	
+}
 
 program ejercicio10;
-const
-	dimf = 300;
+const 
+	dimF = 300;
 type
-	subEmp = 1..dimf;
-	vecSalarios = array[subEmp] of real;
-procedure cargarVector(var v: vecSalarios; var diml: integer);
-var
-	salario: real;
+	empleado = record
+		nombre:string;
+		apellido: string;
+		salario: real;
+	end;
+	empleados = array [1..dimF] of empleado;
+
+procedure leerEmpleado(var p:empleado);
 begin
-	writeln('Ingrese el salario de un empleado');
-	readln(salario);
-	while(diml < dimf) and (salario <> 0) do
-		begin
-			diml:= diml +1;
-			v[diml]:= salario;
-			writeln('Ingrese el salario de un empleado');
-			readln(salario);
-		end;
+	write('Ingrese el salario del empleado: ');
+	readln(p.salario);
+	if(p.salario <> 0) then
+	begin
+		write('Ingrese el nombre: ');
+		readln(p.nombre);
+		write('Ingrese el apellido: ');
+		readln(p.apellido);
+	end;
 end;
-procedure incrementarSalarios(var v: vecSalarios; diml: integer; x: real);
+procedure almacenarEmpleado(var v: empleados; var dimL: integer);
 var
-	i: subEmp;
-    porcentaje: real;
+	e:empleado;
+	i:integer;
 begin
-	porcentaje:= x/100 + 1.5;
-	for i:= 1 to diml do
-		begin
-			v[i]:= v[i] * porcentaje;
-		end;
+	writeln('Vamos a registrar empleados, el programa finaliza al ingresar el salario 0');
+	leerEmpleado(e);
+	while((e.salario <> 0) and (dimL < dimF)) do
+	begin
+	    dimL := dimL + 1;
+		v[dimL] := e;
+		leerEmpleado(e);
+	end;
 end;
-procedure imprimirVector(v: vecSalarios; diml: integer);
-var
-	i: subEmp;
+procedure incisoA(var v: empleados; dimL: integer; num: real);
+var 
+    i: integer;
 begin
-	for i:= 1 to diml do
-		writeln('Salario empleado ', i, '=',v[i]:0:2);
+    for i := 1 to dimL do
+    begin
+        v[i].salario := v[i].salario * num;
+    end;
+end;
+procedure incisoB(v: empleados; dimL:integer; var promedio: real);
+var 
+    i: integer;
+    sumaSalarios: real;
+begin
+    sumaSalarios := 0;
+    for i := 1 to dimL do
+    begin
+        sumaSalarios := sumaSalarios + v[i].salario;
+    end;
+    promedio := sumaSalarios / dimL;
 end;
 var
-	v: vecSalarios;
-	diml: integer;
-    x: real;
+	i, dimL: integer;
+	v: empleados;
+	num, promedio: real;
 begin
-	diml:= 0;
-	cargarVector(v, diml);
-	writeln('Ingrese un valor real X para incrementar salarios');
-	readln(x);
-	incrementarSalarios(v, diml, x);
-	imprimirVector(v, diml);
-end.
+	dimL:= 0;
+	almacenarEmpleado(v, dimL);
+	write('Ingrese un valor para incrementar el salario de los empleados: ');
+	readln(num);
+	incisoA(v, dimL, num);
+	for i := 1 to dimL do
+    begin
+        writeln('Empleado: ', v[i].nombre, ' ,su salario es de: ', v[i].salario:0:2);
+    end;
+    incisoB(v, dimL, promedio);
+    writeln('El salario promedio de los empleados es: ', promedio:0:2);
+End.
