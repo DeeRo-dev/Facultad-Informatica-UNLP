8655a6f9b9e33df4ee78dea0019a46a2fb970750
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-06-19 12:28:36 -0300

    Practica 4, ejercicio: 12

--- 1er-semestre/CADP/Practica/4-1/ejercicio12.pas
+++ 1er-semestre/CADP/Practica/4-1/ejercicio12.pas
@@ -80,18 +80,74 @@ begin
 		totalMasa := totalMasa + v[i].masa;
 	end;
 end;
+function incisoC(v: almGalaxias): integer;
+var
+	 aux, i: integer;
+begin
+	aux	:= 0;
+	for i := 1 to cantGalaxias do
+	begin
+		if((v[i].tipo <> 4) and (v[i].pc < 1000) ) then
+		begin
+			aux := aux + 1;
+		end;
+	end;
+	incisoC := aux;
+end;
+procedure incisoD(v: almGalaxias; var nombreMin1: string; var nombreMin2: string; var nombreMax1: string; var nombreMax2: string);
+-- d) El nombre de las dos galaxias con mayor masa y el de las dos galaxias con menor masa.--
+var
+	min1, min2, max1, max2 ,i:nteger;
+begin
+	min1 : 99999; 
+	min2 := 9999;
+	max1 := -2; 
+	max2 := -1;
+	for i := 1 to cantGalaxias do
+	begin
+		if(v[i].masa < min1) then
+		begin
+			min2 := min1;
+			nombreMin2 := nombreMin1;
+			min1 := v[i].masa;
+			nombreMin1 := v[i].nombre;
+		end
+		else if(v[i] < min2) then
+		begin
+			min2 := v[i].masa;
+			nombreMin2 := v[i].nombre;
+		end;
+		if(v[i].masa > max1) then
+		begin
+			max2 := max1;
+			nombreMax2 := nombreMax1;
+			max1 := v[i].masa;
+			nombreMax1 := v[i].nombre;
+		end
+		else if(v[i] > max2) then
+		begin
+			max2 := v[i].masa;
+			nombreMax2 := v[i].nombre;
+		end;
+	end
+end;
 var
 	datosGalaxias: almGalaxias;
 	datosTipos: tiposGalaxias;
 	porcentaje: real;
-	totalMasa, masaPorTres : integer;
+	totalMasa, masaPorTres, min1, min2, max1, max2 : integer;
+	nombreMin1, nombreMin2, nombreMax1, nombreMax2: string;
 begin
 	masaPorTres := 0;
 	totalMasa := 0;
 	guardarDatosGalaxias(datosGalaxias);
 	incisoA(datosGalaxias, datosTipos);
 	incisoB(datosGalaxias, totalMasa, masaPorTres);
-	writeln('Masa total de las 3 galaxias principales: ', masaPorTres:0:2);
+	writeln('El total de las 3 galaxias principales son de: ', masaPorTres);
 	porcentaje := (masaPorTres / totalMasa) * 100;
-	writeln('Representan el ', porcentaje:0:2, '% de la masa total.');
+	writeln('El porcentaje de las primeras 3 galxias principales es de: ', porcentaje);
+	writeln('La cantidad de galaxias no irregulares que se encuentran a menos de 1000 pc: ', incisoC(datosGalaxias));
+	incisoD(datosGalaxias, nombreMin1, nombreMin2, nombreMax1, nombreMax2);
+	writeln('La galaxia con menor masa es: ', nombreMin1, ', y la segunda es: ', nombreMin2);
+	writeln('La galaxia con mayor masa es: ', nombreMax1, ', y la segunda es: ', nombreMax2);
 End.
