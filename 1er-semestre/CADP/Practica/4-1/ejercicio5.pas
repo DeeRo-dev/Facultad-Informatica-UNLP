d53112dc340ee1533694c32b7e22b6a4aa3a5f4b
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2025-07-14 11:13:01 -0300

    Update ejercicio5.pas

--- 1er-semestre/CADP/Practica/4-1/ejercicio5.pas
+++ 1er-semestre/CADP/Practica/4-1/ejercicio5.pas
@@ -1,81 +1,119 @@
-{5. Utilizando los módulos implementados en el ejercicio 4, realizar un programa que lea números enteros
-desde teclado (a lo sumo 100) y los almacene en un vector. La carga finaliza al leer el número 0. Al finalizar
-la carga, se debe intercambiar la posición del mayor elemento por la del menor elemento, e informe la
-operación realizada de la siguiente manera: “El elemento máximo ... que se encontraba en la posición ...
-fue intercambiado con el elemento mínimo ... que se encontraba en la posición ...”.}
-
-program ejercicio5;
+{
+    3. Una empresa de transporte de caudales desea optimizar el servicio que brinda a sus clientes. Para ello,
+    cuenta con información sobre todos los viajes realizados durante el mes de marzo. De cada viaje se cuenta
+    con la siguiente información: día del mes (de 1 a 31), monto de dinero transportado y distancia recorrida por
+    el camión (medida en kilómetros).
+    
+    a. Realizar un programa que lea y almacene la información de los viajes (a lo sumo 200). La lectura finaliza
+    cuando se ingresa una distancia recorrida igual a 0 km, que no debe procesarse.
+    
+    b. Realizar un módulo que reciba el vector generado en a) e informe:
+    - El monto promedio transportado de los viajes realizados
+    - La distancia recorrida y el día del mes en que se realizó el viaje que transportó menos dinero.
+    - La cantidad de viajes realizados cada día del mes.
+    
+    c. Realizar un módulo que reciba el vector generado en a) y elimine todos los viajes cuya distancia
+    recorrida sea igual a 100 km.
+    
+    Nota: para realizar el inciso b, el vector debe recorrerse una única vez.}
+    
+    
+    
+program ejercicicio3;
 const
-	dimF = 100;
+    totalViajes = 200;
 type
-	vector = array [1..dimF] of integer;
-procedure cargarVector(var v: vector; var dimL: integer); //Se dispone
-var
-	num: integer;
+    cantDias = 1..31;
+    infoViaje = record
+        dia: cantDias;
+        montoDinero: real;
+        km: integer;
+    end;
+    datosViajes = array [1..totalViajes] of infoViaje;
+    mes = array [cantDias] of integer;
+procedure leerViaje(var r: infoViaje);
 begin
-	num:= Random(15);
-	while((dimL < dimF) and (num <> 0)) do
-		begin
-			dimL:= dimL + 1;
-			v[dimL]:= num;
-			num:= Random(15);
-		end;
+    writeln('Ingrese información acerca del viaje: ');
+    write('Ingrese la distancia recorrida: ');
+    readln(r.km);
+    if(r.km <> 0) then
+    begin
+        write('Ingrese el dia en el cual se realizo el viaje (1 a 31): ');
+        readln(r.dia);
+        write('Ingrese el monto promedio que tranporto el vieje: ');
+        readln(r.montoDinero);
+    end;
+    
+   
 end;
-procedure imprimirVector(v: vector; dimL: integer);
+procedure almacenarDatos(var v:datosViajes; var dimL: integer);
 var
-	i: integer;
+   r: infoViaje; 
 begin
-	for i:= 1 to dimL do
-		write(v[i], ' | ');
+    leerViaje(r);
+   while (r.km <> 0) and (dimL < totalViajes) do
+    begin
+        dimL := dimL + 1;
+        v[dimL] := r; 
+        leerViaje(r);
+    end;
 end;
-procedure intercambio(var v: vector; x, y: integer);
-var
-	aux: integer;
+procedure calcularPromedio(v: datosViajes; dimL: integer;  var m: mes);
+var 
+    i, menorDia, menorKm : integer;
+    promedio, totalMonto, menorDinero: real;
+    
 begin
-	aux:= v[x];
-	v[x]:= v[y];
-	v[y]:= aux;
+    totalMonto := 0;
+    menorDinero := 9999;
+   
+    for i := 1 to dimL do
+    begin
+        totalMonto := totalMonto + v[i].montoDinero;
+        if(v[i].montoDinero < menorDinero) then
+        begin
+            menorKm := v[i].km;
+            menorDinero := v[i].montoDinero;
+            menorDia := v[i].dia;
+        end;
+        m[v[i].dia] := m[v[i].dia] + 1;
+    end;
+    promedio := totalMonto / dimL;
+    writeln('El monto promedio es de: ', promedio);
+    writeln('La distancia recorrida del mes en que se realizó el viaje que transportó menos dinero: ', menorKm, ' , el dia', menorDia);
+    for i := 1 to 31 do 
+        writeln('En el dia ' , i , ' la cantidad de viajes fueron: ', m[i]);
 end;
-procedure elementoMaximo(v: vector; dimL: integer; var posMax, numMax: integer);
+procedure eliminarOcurrencia(var v: datosViajes; var dimL: integer);
 var
-	i: integer;
+    i, j: integer;
 begin
-	for i:= 1 to dimL do
-		if(v[i] > numMax) then
-			begin
-				numMax:= v[i];
-				posMax:= i;
-			end;
+    i := 1;
+    while i <= dimL do
+    begin
+        if v[i].km = 100 then
+        begin
+            for j := i to dimL - 1 do
+                v[j] := v[j + 1];
+            dimL := dimL - 1;
+        end
+        else
+            i := i + 1;  
+    end;
 end;
-procedure elementoMinimo(v: vector; dimL: integer; var posMin, numMin: integer);
+
 var
-	i: integer;
+    d: datosViajes;
+    dimL, i: integer;
+    m: mes;
 begin
-	for i:= 1 to dimL do
-		if(v[i] < numMin) then
-			begin
-				numMin:= v[i];
-				posMin:= i;
-			end;
-end;
-var
-	v: vector;
-	dimL, posMax, posMin, numMax, numMin: integer;
-begin
-    Randomize;
-	dimL:= 0;
-	cargarVector(v, dimL);
-    writeln('DIML=', dimL);
-	imprimirVector(v, dimL);
-	writeln();
-	posMax:= -1;
-	posMin:= -1;
-	numMax:= -9999;
-	numMin:= 9999;
-	elementoMaximo(v, dimL, posMax, numMax);
-	elementoMinimo(v, dimL, posMin, numMin);
-	intercambio(v, posMin, posMax);
-	imprimirVector(v, dimL);
-	writeln();
-	writeln('El elemento maximo ', numMax, ' que se encontraba en la posicion ', posMax, ' fue intercambiado con el elemento minimo ', numMin, ' que se encontraba en la posicion ', posMin);
-end.
+    dimL := 0;
+    almacenarDatos(d, dimL);
+    for i := 1 to 31 do
+        m[i] := 0;
+    calcularPromedio(d, dimL, m);
+        
+End.
+    
+    
+    
