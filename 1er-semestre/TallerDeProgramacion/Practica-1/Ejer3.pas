42bde4538cf1e93ddb0ff81a352f06e9951cef79
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2025-09-03 19:20:50 -0300

    Update Ejer3.pas

--- 1er-semestre/TallerDeProgramacion/Practica-1/Ejer3.pas
+++ 1er-semestre/TallerDeProgramacion/Practica-1/Ejer3.pas
@@ -20,19 +20,105 @@
 }
 
 program ejercicio3;
+const
+    limit = 8;
 type 
-    generos : 1..8;
+    generos = 1..limit;
     
     pelicula = record
         codigoPel: integer;
         codigoGen: generos;
+        puntaje: real;
     end;
-    peliculas : ^dataPelicula;
-    dataPelicula = record;
-        dato : dataPelicula;
+    
+    peliculas = ^dataPelicula;
+    dataPelicula = record
+        dato : pelicula;
         sig : peliculas;
     end;
     
-begin
+    dataGeneros = array [generos] of integer;
     
-End;
+procedure leerPelicula(var r: pelicula);
+begin
+    writeln('----- Vamos a leer informacion de la pelicula -----');
+    write('Ingrese el codigo de pelicula: ');
+    readln(r.codigoPel);
+    if(r.codigoPel <> -1 ) then
+    begin
+        write('Ingrese el codigo de genero: ');
+        readln(r.codigoGen);
+        write('Ingrese el puntaje promedio: ');
+        readln(r.puntaje);
+    end;
+end;
+
+procedure armarNodo(var l: peliculas; r: pelicula);
+var
+    nuevo: peliculas;
+begin
+    new(nuevo);
+    nuevo^.dato:= r;
+    nuevo^.sig:=l;
+    l:=nuevo;
+end;
+
+procedure cargarInfo(var l: peliculas);
+var
+    r : pelicula;
+begin
+    leerPelicula(r);
+    while(r.codigoPel <> -1 ) do
+    begin
+        armarNodo(l, r);
+        leerPelicula(r);
+    end;
+end;
+procedure imprimirPeliculas(l: peliculas);
+begin
+    while(l <> nil) do
+    begin
+        writeln;
+        writeln('------ Datos de la pelicula -----');
+        writeln('Codigo de la pelicula: ', l^.dato.codigoPel);
+        writeln('Codigo del genero: ', l^.dato.codigoGen);
+        writeln('Puntje promedio: ', l^.dato.puntaje:0:2);
+        l:=l^.sig;
+    end;
+end;
+
+procedure incisoB(l : peliculas; var g : dataGeneros);
+var
+    gen: generos;
+begin
+    // Inicializar el vector con valores mínimos
+    for gen := 1 to limit do
+    begin
+        g[gen].puntaje := -1; // valor muy bajo
+        g[gen].codigoPel := -1;
+    end;
+
+    // Recorrer la lista una sola vez
+    while (l <> nil) do
+    begin
+        gen := l^.dato.codigoGen;
+        if (l^.dato.puntaje > g[gen].puntaje) then
+        begin
+            g[gen].puntaje := l^.dato.puntaje;
+            g[gen].codigoPel := l^.dato.codigoPel;
+        end;
+        l := l^.sig;
+    end;
+end;
+var
+    l : peliculas;
+    g :dataGeneros;
+    i:integer;
+begin
+    l := nil;
+    cargarInfo(l);
+    imprimirPeliculas(l);
+    for i := 1 to limit do
+        g[i] := -1;
+    incisoB(l, g);
+End.
